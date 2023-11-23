b826299d4b057bef54136a72479d494ac59c4072
daw1FPD - Fontán Padín <A23BeatrizFP@sanclemente.local>
2023-11-23 12:50:59 +0100

    bases 23-11-23

---
+++ Bases de datos/Consultas/3 - Empresa/Consultas_Empresa.sql
@@ -0,0 +1,38 @@
+use EMPRESA_BEA;
+
+--– Proposta 15. Nome de todos os clientes por orde alfabética. 
+
+select c.nome
+from CLIENTE c
+order by nome ASC;
+
+--– Proposta 16. Nome das rexións nas que ten sucursais a empresa.
+
+select s.rexion
+from SUCURSAL s;
+
+
+--– Proposta 17. Identificador dos produtos que nos pediron nalgún momento. 
+--No resultado debe aparecer nunha soa columna o código do fabricante e o identificador 
+--do produto separados por un guión. A columna do resultado deberá chamarse produtos.
+
+select p.cod_fabricante + '-' + p.id_produto as productos
+from PEDIDO p
+where p.id_produto is not null;
+
+
+--– Proposta 18. Información completa das sucursais non dirixidas polo empregado 
+--número 108.
+--– Proposta 19. Nome e límite de crédito do cliente número 1107.
+--– Proposta 20. Número e data dos pedidos feitos entre o 1 de agosto e o 31 de 
+--decembro de 2014. Só debe aparecer a data de cada pedido, sen a hora, co formato 
+--dd-mm-aaaa. Deben aparecer primeiro no resultado os pedidos máis recentes. Para 
+--resolver esta consulta non se poden utilizar operadores de comparación (>, <, >=, 
+--<=, < >, !=).
+--– Proposta 21. Código e nome dos fabricantes cuxo nome ten por segunda letra O.
+--– Proposta 22. Descrición e prezo dos produtos dos que non temos existencias.
+--– Proposta 23. Número identificador e nome completo dos empregados que non 
+--teñen xefe.
+--– Proposta 24. Descrición e unidades existentes, dos produtos con existencias maiores 
+--de 10 unidades e menores de 100. Para resolver esta consulta non se poden utilizar 
+--operadores de comparación (>, <, >=, <=, < >, !=)
