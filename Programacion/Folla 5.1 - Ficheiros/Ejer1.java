a697852975ee6f8a05426b48c30d0020b3e1f1ff
Bea <a23BeatrizFP@iessanclemente.net>
2024-02-20 10:18:59 +0100

    prog 20-02

--- Programacion/Folla 5.1 - Ficheiros/Ejer1.java
+++ Programacion/Folla 5.1 - Ficheiros/Ejer1.java
@@ -1,8 +1,9 @@
-//1. Cal sería o código para saber se o indicado para saber se o indicado no path é un ficheiro? E se é
+//1. Cal sería o código indicado para saber se o indicado no path é un ficheiro? E se é
 //posible escribir no mesmo? Fai un programa que vaia pedindo o nome dun ficheiro, e mostre se existe e
 //se é posible escribir no mesmo. Faino coa clase File, e coa API NIO.
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -15,7 +16,7 @@ public class Ejer1 {
         Scanner teclado = new Scanner(System.in);
 
 
-        System.out.println("Dime o ficheiro que queres buscar");
+        System.out.println("Dime o ficheiro que queres buscar con File");
         nombre = teclado.nextLine();
 
         while (!nombre.equals("X")) {
@@ -29,23 +30,47 @@ public class Ejer1 {
                 System.out.println("El fichero existe");
             }
 
+            //Comprobar si el fichero se puede escribir
+            if (nombreArchivo.canWrite() == true){
+                System.out.println("El fichero se puede escribir");
+            }
+            else{
+                System.out.println("El fichero no se puede escribir");
+            }
+
+
             System.out.println("Dime o ficheiro que queres buscar");
             nombre = teclado.nextLine();
         }
 
 
-        System.out.println("Dime o ficheiro que queres buscar");
+        System.out.println("Dime o ficheiro que queres buscar con Path");
         nombre = teclado.nextLine();
 
         while (!nombre.equals("Y")) {
         
             Path nombreArchivo = Paths.get(nombre);
 
-            if (Files.exists(nombreArchivo)) {
+            if (!Files.exists(nombreArchivo)) {
                 System.out.println("El fichero no existe");
             }
             else{
                 System.out.println("El fichero existe");
+            }
+
+            //Comprobar si el fichero se puede escribir
+            try {
+                Files.isWritable(nombreArchivo);
+            } catch (Exception e) {
+                System.out.println(e.getMessage() + "Problema al intentar ver si el archivo es legible");
+            }
+            
+
+            if (Files.isWritable(nombreArchivo) == true){
+                System.out.println("El fichero se puede escribir");
+            }
+            else{
+                System.out.println("El fichero no se puede escribir");
             }
 
             System.out.println("Dime o ficheiro que queres buscar");
---
+++ Programacion/Folla 5.1 - Ficheiros/Ejer3.java
@@ -0,0 +1,29 @@
+//3. Simplifica o código da introdución UD5.- Ficheiros, para empregar o 
+//try-with-resources. Lembra que o try-with-resources pode ter un bloque catch 
+//se é preciso capturar a excepción: o que non é preciso é
+//empregar o bloque finally para liberar os recursos.
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Ejer3{
+
+    public static void main(String[] args) throws IOException{
+        
+        Scanner scanner = null;
+
+        try (scanner = new Scanner(new File("test.txt"))){
+        
+            while (scanner.hasNext()) {
+                System.out.println(scanner.nextLine());
+            }
+
+
+        } 
+        
+
+
+    }  
+}
